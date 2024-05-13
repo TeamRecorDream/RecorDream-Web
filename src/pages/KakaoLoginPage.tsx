896a11f1b515c3edd4cdb4f21577ed802c9b6c8b
urjimyu <92876819+urjimyu@users.noreply.github.com>
2024-05-13 20:14:21 +0900

    chore: solved build error

--- src/pages/KakaoLoginPage.tsx
+++ src/pages/KakaoLoginPage.tsx
@@ -1,11 +1,11 @@
-import { useState } from "react";
+// import { useState } from "react";
 
 const KakaoLoginPage = () => {
   const code = new URL(window.location.href).searchParams.get("code");
   console.log("CODE", code);
-  const [accessToken, setAccessToken] = useState(localStorage.getItem("accessToken"));
-  console.log("ACCESS_TOKEN", accessToken);
-  if (accessToken) return;
+  // const [accessToken, setAccessToken] = useState(localStorage.getItem("accessToken"));
+  // console.log("ACCESS_TOKEN", accessToken);
+  // if (accessToken) return;
   return <></>;
 };
 
